911c25b9fa78364bb96a14a0d326272fea27eecf
JL <64157982+gitlucc@users.noreply.github.com>
2023-05-12 13:59:44 +0200

    Add files via upload

---
+++ extensions/xExtension-RedditImage/i18n/de/ext.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'reddit_image' => array(
+        'display_image' => 'Bilder anzeigen',
+        'display_original' => 'Originalinhalt anzeigen',
+        'display_video' => 'Videos anzeigen',
+        'image_height' => 'Max. Höhe der Mediendatei',
+        'image_height_unit' => '%% des Darstellungsbereichs (Viewport)',
+        'muted_video' => 'Videos stummschalten',
+    ),
+);
---
+++ extensions/xExtension-RedditImage/i18n/fr/ext.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'reddit_image' => array(
+        'display_image' => 'Afficher les images',
+        'display_original' => 'Afficher le contenu initial',
+        'display_video' => 'Afficher les vidéos',
+        'image_height' => 'Hauteur des médias',
+        'image_height_unit' => '%% de la fenêtre',
+        'muted_video' => 'Vidéo silencieuse',
+    ),
+);
